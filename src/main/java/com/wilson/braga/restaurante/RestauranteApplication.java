1e4b35697920f5fb3d384b5377d69aa6c59c6b13
Wilson Braga <wilsonbraga>
2025-03-14 20:58:06 -0300

    configuracao inicial do projeto

---
+++ src/main/java/com/wilson/braga/restaurante/RestauranteApplication.java
@@ -0,0 +1,13 @@
+package com.wilson.braga.restaurante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestauranteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestauranteApplication.class, args);
+	}
+
+}
