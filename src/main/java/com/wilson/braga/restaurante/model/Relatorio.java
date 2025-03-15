b809e4c38b57ff9cf39b1a60508d60c5c911b391
Wilson Braga <wilsonbraga>
2025-03-14 22:10:35 -0300

    Classe Relatorio e enum TipoRelatorio

---
+++ src/main/java/com/wilson/braga/restaurante/model/Relatorio.java
@@ -0,0 +1,95 @@
+package com.wilson.braga.restaurante.model;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Relatorio implements Serializable {
+
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private TipoRelatorio tipo;
+
+	@Column(nullable = false)
+	private LocalDate dataInicial;
+
+	@Column(nullable = false)
+	private LocalDate dataFim;
+
+	@Column(nullable = false)
+	private double total;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public TipoRelatorio getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(TipoRelatorio tipo) {
+		this.tipo = tipo;
+	}
+
+	public LocalDate getDataInicial() {
+		return dataInicial;
+	}
+
+	public void setDataInicial(LocalDate dataInicial) {
+		this.dataInicial = dataInicial;
+	}
+
+	public LocalDate getDataFim() {
+		return dataFim;
+	}
+
+	public void setDataFim(LocalDate dataFim) {
+		this.dataFim = dataFim;
+	}
+
+	public double getTotal() {
+		return total;
+	}
+
+	public void setTotal(double total) {
+		this.total = total;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Relatorio other = (Relatorio) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
---
+++ src/main/java/com/wilson/braga/restaurante/model/TipoRelatorio.java
@@ -0,0 +1,5 @@
+package com.wilson.braga.restaurante.model;
+
+public enum TipoRelatorio {
+	 DIARIO, MENSAL, ANUAL
+}
