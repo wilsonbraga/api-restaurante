f159067d7bf8501ee6d221c4da01d5ab14694ab6
Wilson Braga <wilsonbraga>
2025-03-14 22:04:22 -0300

    Classe Pagamento e enum FormaPagamento

---
+++ src/main/java/com/wilson/braga/restaurante/model/FormaPagamento.java
@@ -0,0 +1,5 @@
+package com.wilson.braga.restaurante.model;
+
+public enum FormaPagamento {
+	DINHEIRO, CARTAO, PIX
+}
---
+++ src/main/java/com/wilson/braga/restaurante/model/Pagamento.java
@@ -0,0 +1,87 @@
+package com.wilson.braga.restaurante.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Pagamento implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne
+	@JoinColumn(name = "venda_id", nullable = false)
+	private Venda venda;
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private FormaPagamento formaPagamento;
+
+	@Column(nullable = false)
+	private Double valor;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Venda getVenda() {
+		return venda;
+	}
+
+	public void setVenda(Venda venda) {
+		this.venda = venda;
+	}
+
+	public FormaPagamento getFormaPagamento() {
+		return formaPagamento;
+	}
+
+	public void setFormaPagamento(FormaPagamento formaPagamento) {
+		this.formaPagamento = formaPagamento;
+	}
+
+	public Double getValor() {
+		return valor;
+	}
+
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pagamento other = (Pagamento) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+}
