8dd371b974ae4bd6623b2af099e47020ed95e834
Wilson Braga <wilsonbraga>
2025-03-14 21:23:56 -0300

    Classe Mesa e Enum StatusMesa

---
+++ src/main/java/com/wilson/braga/restaurante/model/Mesa.java
@@ -0,0 +1,85 @@
+package com.wilson.braga.restaurante.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Mesa implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false, unique = true)
+	private String numero;
+
+	@Column(nullable = false)
+	private int capacidade;
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private StatusMesa status;
+
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public int getCapacidade() {
+		return capacidade;
+	}
+
+	public void setCapacidade(int capacidade) {
+		this.capacidade = capacidade;
+	}
+
+	public StatusMesa getStatus() {
+		return status;
+	}
+
+	public void setStatus(StatusMesa status) {
+		this.status = status;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Mesa other = (Mesa) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+
+}
---
+++ src/main/java/com/wilson/braga/restaurante/model/StatusMesa.java
@@ -0,0 +1,5 @@
+package com.wilson.braga.restaurante.model;
+
+public enum StatusMesa {
+	DISPONIVEL, OCUPADA
+}
