5e97f7612bd1436ce63b57f049ec8bd2b4acacb7
Wilson Braga <wilsonbraga>
2025-03-15 17:03:33 -0300

    adicionado o Optional no metodo findByEmail e foi criado o findAll

--- src/main/java/com/wilson/braga/restaurante/repository/UsuarioRepository.java
+++ src/main/java/com/wilson/braga/restaurante/repository/UsuarioRepository.java
@@ -1,5 +1,9 @@
 package com.wilson.braga.restaurante.repository;
 
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +12,8 @@ import com.wilson.braga.restaurante.model.Usuario;
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 	
-	Usuario findByEmail(String email); // Buscar usuário por email
+	Optional<Usuario> findByEmail(String email); // Buscar usuário por email
+	
+	Page<Usuario> findAll(Pageable pageable); // Buscar Usuario por Paginação
 
 }
