4ecc5f89c3ec468a46ca8498df4973ca37a3b6fa
Wilson Braga <wilsonbraga>
2025-03-15 20:44:52 -0300

    criacao da class ErrorResponse para especificar melhor a excecao

---
+++ src/main/java/com/wilson/braga/restaurante/exception/ErrorResponse.java
@@ -0,0 +1,64 @@
+package com.wilson.braga.restaurante.exception;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+
+public class ErrorResponse {
+	
+	private LocalDateTime timestamp;
+    private int status;
+    private String error;
+    private String message;
+    private String path;
+    
+    public ErrorResponse(HttpStatus status, String message, String path) {
+        this.timestamp = LocalDateTime.now();
+        this.status = status.value();
+        this.error = status.getReasonPhrase();
+        this.message = message;
+        this.path = path;
+    }
+
+	public LocalDateTime getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(LocalDateTime timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+	public String getError() {
+		return error;
+	}
+
+	public void setError(String error) {
+		this.error = error;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+    
+    
+}
