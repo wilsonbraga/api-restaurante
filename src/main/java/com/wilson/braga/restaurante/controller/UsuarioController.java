bd0a29dae23144790589b55df60696dc11ff6911
Wilson Braga <wilsonbraga>
2025-03-16 15:12:01 -0300

    criacao do metodo buscarUsuarioPorId

--- src/main/java/com/wilson/braga/restaurante/controller/UsuarioController.java
+++ src/main/java/com/wilson/braga/restaurante/controller/UsuarioController.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -78,6 +79,13 @@ public class UsuarioController {
 		// Retorna o usuário cadastrado com status HTTP 201 (Created)
 		return new ResponseEntity<>(usuarioSalvo, HttpStatus.CREATED);
 
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<UsuarioDTO> buscarUsuarioPorId(@PathVariable Long id){
+		return usuarioService.buscarUsuarioPorId(id)
+				.map(ResponseEntity::ok)  // Retorna 200 OK com o usuário
+				.orElseGet(() -> ResponseEntity.notFound().build()); // Retorna 404 Not Found se o usuário não existir
 	}
 
 }
