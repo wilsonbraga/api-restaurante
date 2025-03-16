3651a6ce894dea2a27e54f0825c4fe426d0f38a6
Wilson Braga <wilsonbraga>
2025-03-16 15:04:41 -0300

    removi o metodo buscarUsuarioPorId

--- src/main/java/com/wilson/braga/restaurante/repository/UsuarioRepository.java
+++ src/main/java/com/wilson/braga/restaurante/repository/UsuarioRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.wilson.braga.restaurante.dto.UsuarioDTO;
 import com.wilson.braga.restaurante.model.Usuario;
 
 @Repository
@@ -17,6 +16,5 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 	
 	Page<Usuario> findAll(Pageable pageable); // Buscar Usuario por Paginação
 	
-	Optional<UsuarioDTO> buscarUsuarioPorId(Long id);
 
 }
