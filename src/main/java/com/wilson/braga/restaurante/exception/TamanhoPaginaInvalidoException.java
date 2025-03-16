91d0315bb65b05f54eac0d2c02ba65ee227054ae
Wilson Braga <wilsonbraga>
2025-03-16 14:42:26 -0300

    criacao da classe para excecao personalizada

---
+++ src/main/java/com/wilson/braga/restaurante/exception/TamanhoPaginaInvalidoException.java
@@ -0,0 +1,11 @@
+package com.wilson.braga.restaurante.exception;
+
+public class TamanhoPaginaInvalidoException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public TamanhoPaginaInvalidoException(String message) {
+		super(message);
+	}
+
+}
