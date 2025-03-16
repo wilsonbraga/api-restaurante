3f7f3e3c541ca374c5834745c0f0a65c2ff529a5
Wilson Braga <wilsonbraga>
2025-03-16 14:45:02 -0300

    criei os metodos handleTamanhoPaginaInvalidoException e handleOrdenacaoInvalidaException

--- src/main/java/com/wilson/braga/restaurante/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/wilson/braga/restaurante/exception/handler/GlobalExceptionHandler.java
@@ -12,6 +12,8 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.wilson.braga.restaurante.exception.ErrorResponse;
+import com.wilson.braga.restaurante.exception.OrdenacaoInvalidaException;
+import com.wilson.braga.restaurante.exception.TamanhoPaginaInvalidoException;
 
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
@@ -22,22 +24,24 @@ public class GlobalExceptionHandler {
 	// TRATA EXCEÇÕES QUANDO NAO ENCONTRAR INFORMACAO NO BANCO
 	@ExceptionHandler(ResponseStatusException.class)
 	public ResponseEntity<ErrorResponse> handleResponseStatusException(ResponseStatusException ex, WebRequest request) {
-		
+
 		// Obtém o HttpStatus da exceção
 		HttpStatus status = HttpStatus.valueOf(ex.getStatusCode().value());
-		
-	    ErrorResponse errorResponse = new ErrorResponse(status, ex.getReason() != null ? ex.getReason() : ex.getMessage(), // Mensagem de erro
-	            request.getDescription(false));
+
+		ErrorResponse errorResponse = new ErrorResponse(status,
+				ex.getReason() != null ? ex.getReason() : ex.getMessage(), // Mensagem de erro
+				request.getDescription(false));
 
 		return new ResponseEntity<>(errorResponse, status);
 	}
-	
+
 	// TRATA EXCEÇÕES DO TIPO: ANOTAÇÕES COMO @NOTNULL, @SIZE, @MIN DO JAKARTA BEAN
 	// VALIDATION
-	
+
 	@ExceptionHandler(ConstraintViolationException.class)
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	public ResponseEntity<Map<String, String>> handleConstraintViolationException(ConstraintViolationException ex) {
+		
 		Map<String, String> errors = new HashMap<>();
 
 		for (ConstraintViolation<?> violation : ex.getConstraintViolations()) {
@@ -46,6 +50,25 @@ public class GlobalExceptionHandler {
 
 		return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
 	}
-	
-	 
+
+	@ExceptionHandler(TamanhoPaginaInvalidoException.class)
+	public ResponseEntity<ErrorResponse> handleTamanhoPaginaInvalidoException(TamanhoPaginaInvalidoException ex,
+			WebRequest request) {
+
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+
+		ErrorResponse errorResponse = new ErrorResponse(status, ex.getMessage(), request.getDescription(false));
+		return new ResponseEntity<>(errorResponse, status);
+	}
+
+	@ExceptionHandler(OrdenacaoInvalidaException.class)
+	public ResponseEntity<ErrorResponse> handleOrdenacaoInvalidaException(OrdenacaoInvalidaException ex,
+			WebRequest request) {
+		
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		
+		ErrorResponse errorResponse = new ErrorResponse(status, ex.getMessage(), request.getDescription(false));
+		return new ResponseEntity<>(errorResponse, status);
+	}
+
 }
