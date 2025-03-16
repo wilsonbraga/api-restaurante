1cd2dd6ec00883c985fa240e9733908f9125fc00
Wilson Braga <wilsonbraga>
2025-03-16 14:58:53 -0300

    criei o metodo buscarUsuarioPorId

--- src/main/java/com/wilson/braga/restaurante/repository/UsuarioRepository.java
+++ src/main/java/com/wilson/braga/restaurante/repository/UsuarioRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.wilson.braga.restaurante.dto.UsuarioDTO;
 import com.wilson.braga.restaurante.model.Usuario;
 
 @Repository
@@ -15,5 +16,7 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 	Optional<Usuario> findByEmail(String email); // Buscar usuário por email
 	
 	Page<Usuario> findAll(Pageable pageable); // Buscar Usuario por Paginação
+	
+	Optional<UsuarioDTO> buscarUsuarioPorId(Long id);
 
 }
