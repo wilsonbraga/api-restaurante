11f55e34a2f26ad8fd369b9edb93c4786f301846
Wilson Braga <wilsonbraga>
2025-03-17 20:03:38 -0300

    adicionei alguns atributos para trabalhar com data e criei o meto prePersist, preUpdate e getTotal

--- src/main/java/com/wilson/braga/restaurante/model/Pedido.java
+++ src/main/java/com/wilson/braga/restaurante/model/Pedido.java
@@ -1,6 +1,7 @@
 package com.wilson.braga.restaurante.model;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Objects;
 
@@ -15,6 +16,8 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 
 @Entity
 public class Pedido implements Serializable {
@@ -34,13 +37,55 @@ public class Pedido implements Serializable {
 	@Enumerated(EnumType.STRING)
 	@Column(nullable = false)
 	private StatusPedido status;
-	
+
 	@OneToMany(mappedBy = "pedido", cascade = CascadeType.ALL)
 	private List<ItemPedido> itens;
-	
+
 	@ManyToOne
-    @JoinColumn(name = "garcom_id")
-    private Usuario garcom;
+	@JoinColumn(name = "garcom_id")
+	private Usuario garcom;
+
+	@Column(nullable = false)
+	private LocalDateTime dataCriacao;
+
+	@Column
+	private LocalDateTime dataAtualizacao;
+
+	@Column
+	private LocalDateTime dataPreparo;
+
+	@Column
+	private LocalDateTime dataPronto;
+
+	@Column
+	private LocalDateTime dataEntrega;
+
+	@Column
+	private String observacoes;
+
+	@PrePersist
+	public void prePersist() {
+		this.dataCriacao = LocalDateTime.now();
+		if (this.status == null) {
+			this.status = StatusPedido.EM_PREPARO;
+		}
+
+		if (this.status == StatusPedido.EM_PREPARO) {
+			this.dataPreparo = this.dataCriacao;
+		}
+	}
+
+	@PreUpdate
+	public void preUpdate() {
+		this.dataAtualizacao = LocalDateTime.now();
+
+		// Atualiza os timestamps baseados no status atual
+		if (this.status == StatusPedido.PRONTO && this.dataPronto == null) {
+			this.dataPronto = this.dataAtualizacao;
+		} else if (this.status == StatusPedido.ENTREGUE && this.dataEntrega == null) {
+			this.dataEntrega = this.dataAtualizacao;
+		}
+	}
 
 	public Long getId() {
 		return id;
@@ -81,13 +126,68 @@ public class Pedido implements Serializable {
 	public void setItens(List<ItemPedido> itens) {
 		this.itens = itens;
 	}
-	
+
 	public Usuario getGarcom() {
 		return garcom;
 	}
-	
+
 	public void setGarcom(Usuario garcom) {
 		this.garcom = garcom;
+	}
+
+	public LocalDateTime getDataCriacao() {
+		return dataCriacao;
+	}
+
+	public void setDataCriacao(LocalDateTime dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+
+	public LocalDateTime getDataAtualizacao() {
+		return dataAtualizacao;
+	}
+
+	public void setDataAtualizacao(LocalDateTime dataAtualizacao) {
+		this.dataAtualizacao = dataAtualizacao;
+	}
+
+	public LocalDateTime getDataPreparo() {
+		return dataPreparo;
+	}
+
+	public void setDataPreparo(LocalDateTime dataPreparo) {
+		this.dataPreparo = dataPreparo;
+	}
+
+	public LocalDateTime getDataPronto() {
+		return dataPronto;
+	}
+
+	public void setDataPronto(LocalDateTime dataPronto) {
+		this.dataPronto = dataPronto;
+	}
+
+	public LocalDateTime getDataEntrega() {
+		return dataEntrega;
+	}
+
+	public void setDataEntrega(LocalDateTime dataEntrega) {
+		this.dataEntrega = dataEntrega;
+	}
+
+	public String getObservacoes() {
+		return observacoes;
+	}
+
+	public void setObservacoes(String observacoes) {
+		this.observacoes = observacoes;
+	}
+	
+	public Double getTotal() {
+		return itens.stream()
+				.mapToDouble(item -> item.getQuantidade() * item.getPrecoUnitario())
+				.sum();
+						
 	}
 
 	@Override
@@ -106,8 +206,5 @@ public class Pedido implements Serializable {
 		Pedido other = (Pedido) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
-	
 
 }
