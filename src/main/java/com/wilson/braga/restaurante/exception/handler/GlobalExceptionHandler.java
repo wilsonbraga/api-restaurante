7bd5fe25aa36144906777628bf6caed003cf7bc7
Wilson Braga <wilsonbraga>
2025-03-17 15:58:38 -0300

    adicionei o metodo handleMethodArgumentTypeMismatch para trata erros de parâmetros incorretos

--- src/main/java/com/wilson/braga/restaurante/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/wilson/braga/restaurante/exception/handler/GlobalExceptionHandler.java
@@ -9,12 +9,14 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.wilson.braga.restaurante.exception.ErrorResponse;
 import com.wilson.braga.restaurante.exception.OrdenacaoInvalidaException;
 import com.wilson.braga.restaurante.exception.TamanhoPaginaInvalidoException;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
 
@@ -41,7 +43,7 @@ public class GlobalExceptionHandler {
 	@ExceptionHandler(ConstraintViolationException.class)
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	public ResponseEntity<Map<String, String>> handleConstraintViolationException(ConstraintViolationException ex) {
-		
+
 		Map<String, String> errors = new HashMap<>();
 
 		for (ConstraintViolation<?> violation : ex.getConstraintViolations()) {
@@ -64,11 +66,25 @@ public class GlobalExceptionHandler {
 	@ExceptionHandler(OrdenacaoInvalidaException.class)
 	public ResponseEntity<ErrorResponse> handleOrdenacaoInvalidaException(OrdenacaoInvalidaException ex,
 			WebRequest request) {
-		
+
 		HttpStatus status = HttpStatus.BAD_REQUEST;
-		
+
 		ErrorResponse errorResponse = new ErrorResponse(status, ex.getMessage(), request.getDescription(false));
 		return new ResponseEntity<>(errorResponse, status);
+	}
+
+	// Trata erros de parâmetros incorretos (ex.: URL com tipo errado)
+	@ExceptionHandler(MethodArgumentTypeMismatchException.class)
+	public ResponseEntity<String> handleMethodArgumentTypeMismatch(MethodArgumentTypeMismatchException ex,
+			HttpServletRequest request) {
+
+		String message = String.format(
+				"Erro na URL para o método '%s': O parâmetro '%s' deve ser do tipo '%s'. Valor fornecido: '%s'.",
+				request.getMethod(), // Verbo HTTP
+				ex.getName(), // Nome do parâmetro
+				ex.getRequiredType().getSimpleName(), // Tipo esperado
+				ex.getValue()); // Valor recebido
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(message);
 	}
 
 }
