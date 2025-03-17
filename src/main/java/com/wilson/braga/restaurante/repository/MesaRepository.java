56ee5fbcad900fbd182fe2c101e841c7cf33d39a
Wilson Braga <wilsonbraga>
2025-03-17 14:20:29 -0300

    adicionei os metodos findByCapacidadeGreaterThanEqual e findByGarcomIdAndStatusMesaOcupada

--- src/main/java/com/wilson/braga/restaurante/repository/MesaRepository.java
+++ src/main/java/com/wilson/braga/restaurante/repository/MesaRepository.java
@@ -3,6 +3,8 @@ package com.wilson.braga.restaurante.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.wilson.braga.restaurante.model.Mesa;
@@ -12,4 +14,13 @@ import com.wilson.braga.restaurante.model.StatusMesa;
 public interface MesaRepository extends JpaRepository<Mesa, Long> {
 	
 	List<Mesa> findByStatus(StatusMesa status); //Buscar mesas por status
+	
+	//Buscar mesas por capacidade mínima:
+	List<Mesa> findByCapacidadeGreaterThanEqual(int capacidade);
+	
+	//Buscar mesas ocupadas por um garçom específico:
+	  @Query("SELECT DISTINCT m FROM Mesa m " +
+	           "JOIN m.pedidos p " +
+	           "WHERE p.garcom.id = :garcomId AND m.status = 'OCUPADA'")
+	List<Mesa> findByGarcomIdAndStatusMesaOcupada(@Param("garcomId") Long garcomId);
 }
