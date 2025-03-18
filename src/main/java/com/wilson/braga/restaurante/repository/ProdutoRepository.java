f7db1682e9b422c455ca5a017b64bce1bb9f24ff
Wilson Braga <wilsonbraga>
2025-03-18 18:01:45 -0300

    adicionei o metodo findTop10ByOrderByTotalVendasDesc é responsavel por buscar os Produtos mais vendidos

--- src/main/java/com/wilson/braga/restaurante/repository/ProdutoRepository.java
+++ src/main/java/com/wilson/braga/restaurante/repository/ProdutoRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.wilson.braga.restaurante.model.CategoriaProduto;
@@ -14,5 +15,8 @@ public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 	
 	List<Produto> findByCategoria(CategoriaProduto categoria, Pageable pageable); // Buscar produtos por categoria por paginacao
 
-	List<Produto> findByDisponivelTrue(Pageable pageable); 
+	List<Produto> findByDisponivelTrue(Pageable pageable);
+	
+	@Query("SELECT p FROM Produto p ORDER BY p.totalVendas DESC")
+	List<Produto> findTop10ByOrderByTotalVendasDesc(); //Buscar produtos mais vendidos (usando uma consulta personalizada com @Query):
 }
