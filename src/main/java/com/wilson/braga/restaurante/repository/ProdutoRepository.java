b54320cde3dbc25096c19a02ca9a6841a20c9187
Wilson Braga <wilsonbraga>
2025-03-18 17:59:13 -0300

    adicionei o metodo findByDisponivelTrue que vai buscar Produtos disponiveis com paginacao

--- src/main/java/com/wilson/braga/restaurante/repository/ProdutoRepository.java
+++ src/main/java/com/wilson/braga/restaurante/repository/ProdutoRepository.java
@@ -2,10 +2,8 @@ package com.wilson.braga.restaurante.repository;
 
 import java.util.List;
 
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.wilson.braga.restaurante.model.CategoriaProduto;
@@ -16,5 +14,5 @@ public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 	
 	List<Produto> findByCategoria(CategoriaProduto categoria, Pageable pageable); // Buscar produtos por categoria por paginacao
 
-
+	List<Produto> findByDisponivelTrue(Pageable pageable); 
 }
