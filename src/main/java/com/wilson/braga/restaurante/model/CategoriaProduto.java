54198ace802dd938c14f76378538115c0d2738b0
Wilson Braga <wilsonbraga>
2025-03-19 00:39:23 -0300

    atualizacao na classe

--- src/main/java/com/wilson/braga/restaurante/model/CategoriaProduto.java
+++ src/main/java/com/wilson/braga/restaurante/model/CategoriaProduto.java
@@ -1,5 +1,17 @@
 package com.wilson.braga.restaurante.model;
 
 public enum CategoriaProduto {
-	BEBIDA, PRATO_PRINCIPAL, SOBREMESA
+	BEBIDA("Bebida"),
+    PRATO_PRINCIPAL("Prato Principal"),
+    SOBREMESA("Sobremesa");
+
+    private final String descricao;
+
+    CategoriaProduto(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
 }
