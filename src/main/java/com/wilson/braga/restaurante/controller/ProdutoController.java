224c594af793ae503a459186ff8d3bdc638be3de
Wilson Braga <wilsonbraga>
2025-03-19 21:35:34 -0300

    criacao do metodo salvarProduto

--- src/main/java/com/wilson/braga/restaurante/controller/ProdutoController.java
+++ src/main/java/com/wilson/braga/restaurante/controller/ProdutoController.java
@@ -1,16 +1,23 @@
 package com.wilson.braga.restaurante.controller;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.wilson.braga.restaurante.dto.ProdutoDTO;
 import com.wilson.braga.restaurante.service.ProdutoService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/produtos")
@@ -26,7 +33,14 @@ public class ProdutoController {
 		return ResponseEntity.ok(produtos);
 	}
 
-	
-	
+	@PostMapping
+	public ResponseEntity<ProdutoDTO> salvarProduto(@Valid @RequestBody ProdutoDTO produtoDTO) {
+		ProdutoDTO novoProduto = produtoService.salvarProduto(produtoDTO);
+
+		// Criar URI para o recurso criado
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(novoProduto.getId())
+				.toUri();
+		return ResponseEntity.created(uri).body(novoProduto);
+	}
 
 }
