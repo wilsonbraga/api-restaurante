43ed3acf8fb39fb87367525cedef4c5d4098aa43
Wilson Braga <wilsonbraga>
2025-03-19 22:01:58 -0300

    criacao do metodo excluirProduto

--- src/main/java/com/wilson/braga/restaurante/controller/ProdutoController.java
+++ src/main/java/com/wilson/braga/restaurante/controller/ProdutoController.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -73,6 +74,12 @@ public class ProdutoController {
 
 		Page<ProdutoDTO> produtos = produtoService.buscarPorCategoria(categoria, pageable);
 		return ResponseEntity.ok(produtos);
+	}
+	
+	@DeleteMapping("/excluir/{id}")
+	public ResponseEntity<Void> excluirProduto(@PathVariable Long id) {
+		produtoService.excluir(id);
+		return ResponseEntity.noContent().build();
 	}
 
 }
