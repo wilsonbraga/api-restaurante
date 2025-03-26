cc9cc4530414a186fd6901932716375e5b567e09
Wilson Braga <wilsonbraga>
2025-03-26 18:34:58 -0300

    implementacao do metodo findByPedidoDataCriacaoBetween

--- src/main/java/com/wilson/braga/restaurante/repository/ItemPedidoRepository.java
+++ src/main/java/com/wilson/braga/restaurante/repository/ItemPedidoRepository.java
@@ -1,11 +1,13 @@
 package com.wilson.braga.restaurante.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.wilson.braga.restaurante.model.ItemPedido;
@@ -30,6 +32,12 @@ public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Long> {
 			+ "GROUP BY i.produto.id, i.produyo.nome "
 			+ "ORDER BY total DESC")
 	List<ItemPedido> findMostOrderedItems(Pageable pageable);
+	
+	// Buscar itens de pedidos em um intervalo de datas
+	@Query("SELECT i FROM ItemPedido i WHERE i.pedido.dataCriacao BETWEEN :dataInicio AND :dataFim")
+	List<ItemPedido> findByPedidoDataCriacaoBetween(
+			@Param("dataInicio") Date dataInicio, 
+			@Param("dataFim") Date dataFim);
 	
 	
 }
