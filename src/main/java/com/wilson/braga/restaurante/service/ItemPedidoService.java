281491894b5242d6d1d14aea72f821b51bc3b76b
Wilson Braga <wilsonbraga>
2025-04-05 23:18:27 -0300

    criacao do metodo findById

--- src/main/java/com/wilson/braga/restaurante/service/ItemPedidoService.java
+++ src/main/java/com/wilson/braga/restaurante/service/ItemPedidoService.java
@@ -4,7 +4,10 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.wilson.braga.restaurante.dto.ItemPedidoDTO;
 import com.wilson.braga.restaurante.model.ItemPedido;
@@ -16,10 +19,21 @@ public class ItemPedidoService {
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
 
-	public List<ItemPedidoDTO> findByPedidoId(Long pedidoId){
+	@Transactional(readOnly = true)
+	public List<ItemPedidoDTO> findByPedidoId(Long pedidoId) {
 		List<ItemPedido> itens = itemPedidoRepository.findByPedidoId(pedidoId);
 		return itens.stream().map(this::convertToDTO).collect(Collectors.toList());
 	}
+
+	@Transactional(readOnly = true)
+	public ItemPedidoDTO findById(Long id) {
+		ItemPedido item = itemPedidoRepository.findById(id).orElseThrow(
+				() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Item pedido não encontrado com ID: " + id));
+		return convertToDTO(item);
+	}
+	
+	
+	
 
 	// Conversão entre DTO e Entidade
 	private ItemPedidoDTO convertToDTO(ItemPedido item) {
