0c22239c6f0010ef08a813f2cbee1ee196d5ac8d
Wilson Braga <wilsonbraga>
2025-04-29 18:54:50 -0300

    criacao da classe RelatorioProdutoDTO

---
+++ src/main/java/com/wilson/braga/restaurante/dto/RelatorioProdutoDTO.java
@@ -0,0 +1,42 @@
+package com.wilson.braga.restaurante.dto;
+
+public class RelatorioProdutoDTO {
+
+	private Long produtoId;
+	private String nomeProduto;
+	private Integer quantidadeTotal;
+	private Double valorTotal;
+
+	public Long getProdutoId() {
+		return produtoId;
+	}
+
+	public void setProdutoId(Long produtoId) {
+		this.produtoId = produtoId;
+	}
+
+	public String getNomeProduto() {
+		return nomeProduto;
+	}
+
+	public void setNomeProduto(String nomeProduto) {
+		this.nomeProduto = nomeProduto;
+	}
+
+	public Integer getQuantidadeTotal() {
+		return quantidadeTotal;
+	}
+
+	public void setQuantidadeTotal(Integer quantidadeTotal) {
+		this.quantidadeTotal = quantidadeTotal;
+	}
+
+	public Double getValorTotal() {
+		return valorTotal;
+	}
+
+	public void setValorTotal(Double valorTotal) {
+		this.valorTotal = valorTotal;
+	}
+
+}
